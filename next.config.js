29321f04f04ee2594eaf7d6eac6b552288a572cb
Suhyun Park <me@shiftpsh.com>
2023-05-03 21:51:51 +0900

    feat(ci): setup CI

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  output: "export",
   experimental: {
     appDir: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
