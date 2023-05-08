9d3699ec340de2f87fd81bd974e33d3a61b4b68d
Suhyun Park <me@shiftpsh.com>
2023-05-08 16:32:55 +0900

    feat: tooltip text

--- app/GamePlayingItem.tsx
+++ app/GamePlayingItem.tsx
@@ -2,7 +2,7 @@ import Emoji from "@/component/Emoji";
 import useElapsedTime from "@/hooks/useElapsedTime";
 import { GameResultStarted } from "@/types/GameResult";
 import { gameById } from "@/utils/game";
-import { Typo } from "@solved-ac/ui-react";
+import { Tooltip, Typo } from "@solved-ac/ui-react";
 
 interface Props {
   data: GameResultStarted;
@@ -12,14 +12,18 @@ const GamePlayingItem = ({ data }: Props) => {
   const elapsedTime = useElapsedTime(data.startedAt);
   const game = gameById(data.gameId);
 
+  const tooltipText = `적정 플레이 시간: ${game.estimatedPlayTime.min} - ${game.estimatedPlayTime.max}분`;
+
   return (
-    <span>
-      <Emoji emoji={game.emoji} /> <span>{game.name}</span>{" "}
-      <Typo description>
-        {Math.floor(elapsedTime / 60 / 1000)}
-        <Typo small>분</Typo>
-      </Typo>
-    </span>
+    <Tooltip title={tooltipText}>
+      <span>
+        <Emoji emoji={game.emoji} /> <span>{game.name}</span>{" "}
+        <Typo description>
+          {Math.floor(elapsedTime / 60 / 1000)}
+          <Typo small>분</Typo>
+        </Typo>
+      </span>
+    </Tooltip>
   );
 };
 
