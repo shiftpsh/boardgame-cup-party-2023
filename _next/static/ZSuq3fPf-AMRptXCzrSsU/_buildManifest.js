bdad34b4499aa9fcdf804c2701fe518722f46929
shiftpsh <shiftpsh@users.noreply.github.com>
2023-05-09 17:49:20 +0000

    deploy: f53de16ce8f102eee41d2284d6d8320e96f4c00c

--- _next/static/ZSuq3fPf-AMRptXCzrSsU/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-db3bc4be91c5c58b.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
