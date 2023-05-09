ba0e48ca5d0d4cba26863ab53927c0233f2d449d
Suhyun Park <me@shiftpsh.com>
2023-05-09 11:32:10 +0900

    feat: freeze

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 
+import useElapsedTime from "@/hooks/useElapsedTime";
+import useFreezeTime from "@/hooks/useFreezeTime";
 import useScoreboard from "@/hooks/useScoreboard";
-import { gameById } from "@/utils/game";
-import { Divider } from "@solved-ac/ui-react";
+import { Divider, Space, Typo } from "@solved-ac/ui-react";
 import { motion } from "framer-motion";
 import React, { useMemo } from "react";
 import GamePlayingItem from "./GamePlayingItem";
@@ -10,6 +11,9 @@ import ScoreboardRow from "./ScoreboardRow";
 
 export default function Home() {
   const scoreboard = useScoreboard();
+  const [freezeTime] = useFreezeTime();
+  const elapsedTime = useElapsedTime(freezeTime);
+  const secondsUntilFreeze = Math.floor(-elapsedTime / 1000);
 
   const gamesPlaying = useMemo(
     () =>
@@ -21,26 +25,49 @@ export default function Home() {
 
   return (
     <>
-      {gamesPlaying.length !== 0 && (
-        <div
-          style={{
-            padding: 16,
-            overflowX: "auto",
-          }}
-        >
-          현재 플레이 중:{" "}
-          {gamesPlaying.map((data, i) => {
-            const { gameId } = data;
-            const game = gameById(data.gameId);
-            return (
-              <React.Fragment key={gameId}>
-                {i !== 0 && ", "}
-                <GamePlayingItem data={data} />
-              </React.Fragment>
-            );
-          })}
-        </div>
-      )}
+      <div
+        style={{
+          padding: 16,
+          overflowX: "auto",
+        }}
+      >
+        {gamesPlaying.length !== 0 && (
+          <>
+            현재 플레이 중:{" "}
+            {gamesPlaying.map((data, i) => {
+              const { gameId } = data;
+              return (
+                <React.Fragment key={gameId}>
+                  {i !== 0 && ", "}
+                  <GamePlayingItem data={data} />
+                </React.Fragment>
+              );
+            })}
+          </>
+        )}
+        <Space h={8} />
+        {120 >= secondsUntilFreeze && secondsUntilFreeze > 60 && (
+          <>
+            <Typo description tabular>
+              {Math.floor(secondsUntilFreeze / 60)}분 후 프리즈
+            </Typo>
+          </>
+        )}
+        {60 >= secondsUntilFreeze && secondsUntilFreeze > 0 && (
+          <>
+            <Typo success tabular>
+              <b>{secondsUntilFreeze}초 후 프리즈</b>
+            </Typo>
+          </>
+        )}
+        {secondsUntilFreeze <= 0 && (
+          <>
+            <Typo success tabular>
+              <b>스코어보드 프리즈됨</b>
+            </Typo>
+          </>
+        )}
+      </div>
       <div
         style={{
           position: "relative",
